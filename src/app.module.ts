fb62ad1a17ba9dfd0ca6166ac893ecb9f4981c8d
coleman <coleman@hidt>
2019-10-14 17:42:06 -0300

    Add: Estructura de error

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,18 @@ import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
 import { Configuration } from './config/config.keys';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
 
 @Module({
   imports: [ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter
+    }
+  ],
 })
 export class AppModule {
   static port: number | string;
